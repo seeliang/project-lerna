dc8c30ce8ab55193002a2325275bc8d78c705e30
seeliang <hi@sihuiliang.com>
2021-10-27 10:57:32 +1100

    chore: add script

---
+++ bash/publish_set.sh
@@ -0,0 +1,6 @@
+  npm config set always-auth true
+  npm config set //npm.pkg.github.com/:_authToken ${GH_TOKEN}
+  npm config set @seeliang:registry https://npm.pkg.github.com
+  npm set email hi@sihuiliang.com
+  git config user.email hi@sihuiliang.com
+  git config user.name seeliang
