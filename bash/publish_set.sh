63296a47367d354fb2ec41417b152a7bba442d49
seeliang <hi@sihuiliang.com>
2022-05-02 12:05:31 +1000

    fix: graduate

--- bash/publish_set.sh
+++ bash/publish_set.sh
@@ -1,4 +1,5 @@
   npm config set always-auth true
+  echo ${GH_PACKAGE_TOKEN}
   npm config set //npm.pkg.github.com/:_authToken ${GH_PACKAGE_TOKEN}
   npm config set @seeliang:registry https://npm.pkg.github.com
   npm set email hi@sihuiliang.com
