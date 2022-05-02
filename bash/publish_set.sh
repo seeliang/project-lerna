76a152af26a6acd00318dc06578eb50b0ec1fa3e
seeliang <hi@sihuiliang.com>
2022-05-02 12:10:42 +1000

    chore: reset

--- bash/publish_set.sh
+++ bash/publish_set.sh
@@ -1,5 +1,4 @@
   npm config set always-auth true
-  echo ${GH_PACKAGE_TOKEN}
   npm config set //npm.pkg.github.com/:_authToken ${GH_PACKAGE_TOKEN}
   npm config set @seeliang:registry https://npm.pkg.github.com
   npm set email hi@sihuiliang.com
--- bash/release.sh
+++ bash/release.sh
@@ -3,7 +3,7 @@
     git checkout $GITHUB_HEAD_REF
     yarn release
   else 
-    # echo "skip release for ${GITHUB_HEAD_REF} branch"
-    git checkout $GITHUB_HEAD_REF
-    yarn release:pre
+    echo "skip release for ${GITHUB_HEAD_REF} branch"
+    # git checkout $GITHUB_HEAD_REF
+    # yarn release:pre
   fi
